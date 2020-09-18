3157b4366f39e22143f8c1fc9aada054af1aede2
shiraalter <shiraalter@gmail.com>
2020-09-17 22:55:20 -0400

    score updates automatically

--- src/main/java/touro/snake/SnakeFrame.java
+++ src/main/java/touro/snake/SnakeFrame.java
@@ -9,9 +9,10 @@ public class SnakeFrame extends JFrame {
 
     public JLabel scoreBoard;
 
+
     public SnakeFrame(GardenView gardenView, SnakeKeyListener snakeKeyListener, Snake snake) throws HeadlessException {     //ADDED SNAKE
         super();
-        setSize(1000,400);
+        setSize(1000, 400);
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         setTitle("Snake");
         setLayout(new BorderLayout());
@@ -19,9 +20,15 @@ public class SnakeFrame extends JFrame {
         add(gardenView, BorderLayout.CENTER);
         addKeyListener(snakeKeyListener);
 
-        scoreBoard = new JLabel("Score");
-        add(scoreBoard, BorderLayout.SOUTH);
+        scoreBoard = new JLabel();
+        ActionListener scoreTracker = actionEvent -> {
+            scoreBoard.setText("Score: " + snake.getScore());
+        };
 
+        //updates score immediately upon eating the food
+        Timer timer = new Timer(0, scoreTracker);
+        timer.start();
+        add(scoreBoard, BorderLayout.SOUTH);
 
 
     }
