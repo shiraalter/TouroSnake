3dea592e11d1688e658515666d2d884bf2f7ce1f
shiraalter <shiraalter@gmail.com>
2020-10-27 22:54:44 -0400

    fixed AStar code

--- src/main/java/touro/snake/strategy/astar/alter/AStarStrategy.java
+++ src/main/java/touro/snake/strategy/astar/alter/AStarStrategy.java
@@ -8,6 +8,7 @@ import touro.snake.strategy.SnakeStrategy;
 import touro.snake.strategy.astar.Node;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 
 
 public class AStarStrategy implements SnakeStrategy {
@@ -17,10 +18,7 @@ public class AStarStrategy implements SnakeStrategy {
         ArrayList<Node> openList = new ArrayList<>();
         ArrayList<Node> closedList = new ArrayList<>();
 
-        Direction direction = null;     //had to initialize
-        Direction directionArray[] = Direction.values();
-
-        Node currentNode;   //temp node
+        Direction[] directionArray = Direction.values();
         Node startNode = new Node(snake.getHead()); //get initial square
         Food targetNode = garden.getFood();
 
@@ -29,60 +27,55 @@ public class AStarStrategy implements SnakeStrategy {
         }
 
         openList.add(startNode);
-        currentNode = openList.get(0);
 
         while (!openList.isEmpty()) {
-
-            currentNode = getCurrentNode(openList, currentNode);
+            Node currentNode = getLowestNode(openList);
             openList.remove(currentNode);
             closedList.add(currentNode);
 
-            if (currentNode.getX() == targetNode.getX() && currentNode.getY() == targetNode.getY()) {
-                 //found path!
-                //create array list to hold the path. Loop through path and get direction
-                ArrayList<Node> path = tracePath(currentNode,startNode);
-                for (Node node : path) {
-                    direction = node.directionTo(startNode);
-                    snake.turnTo(direction);
-                }
+            //found target
+            if (currentNode.equals(targetNode)) {
+                Node childNode = tracePath(currentNode, startNode);
+                Direction direction = startNode.directionTo(childNode);
+                snake.turnTo(direction);
                 break;
             }
+          
+
+            //if target NOT found yet:
+            for (Direction directions : directionArray) {
+                Node adjacentNode = new Node(currentNode.moveTo(directions), currentNode, targetNode);
+                if (closedList.contains(adjacentNode) || !adjacentNode.inBounds() || snake.contains(adjacentNode)) {
+                    continue;   //skip to next neighbor
+                }
+
+               int index = openList.indexOf(adjacentNode);
+                if (index != -1) {
+                    Node oldAdjacent = openList.get(index);
+                    if (adjacentNode.getCost() < oldAdjacent.getCost()) {
+                        openList.remove(index);
+                        openList.add(adjacentNode);
+                    }
+                }
+                    else{
+                        openList.add(adjacentNode);
+                    }
+                }
+            }
+
         }
 
 
-        //if target NOT found:
-        for (Direction directions : directionArray) {
-            Node adjacentNode = new Node(currentNode.moveTo(directions));
-            if (closedList.contains(adjacentNode) || !adjacentNode.inBounds() || snake.contains(adjacentNode)) {
-                continue;   //skip to next neighbor
-            }
-
-            //include if new path to neighbor is shorter..?
-            if (!openList.contains(adjacentNode)) {
-                Node newNode = new Node(adjacentNode, currentNode, targetNode);
-                openList.add(newNode);
-            }
-        }
+    private Node getLowestNode(ArrayList<Node> openList) {
+        return openList.stream().min(Comparator.comparingDouble(Node::getCost)).get();
     }
 
-    private Node getCurrentNode(ArrayList<Node> openList, Node currentNode) {
-        //search open list and let currentNode equal node with least f val
-        for (Node node : openList) {
-            if (node.getCost() < currentNode.getCost()) {
-                currentNode = node;
-            }
+    private Node tracePath(Node currentNode, Node startNode) {
+        Node end = currentNode;
+        while (!end.getParent().equals(startNode)) {    //loop back but stop before it reaches the head
+            end = end.getParent();
         }
-        return currentNode;
-    }
-
-    private ArrayList<Node> tracePath (Node currentNode, Node startNode) {
-        ArrayList<Node> path = new ArrayList<>();
-        while (currentNode.getParent() != startNode) {    //loop back but stop before it reaches the head
-            path.add(currentNode);
-            currentNode = currentNode.getParent();
-        }
-        return path;
-       // return startNode.directionTo(currentNode);
+        return end;
     }
 }
 
