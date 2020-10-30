b8d1afcc24f305c4494100f6e590d8dc386d7d8f
shiraalter <shiraalter@gmail.com>
2020-10-29 23:34:44 -0400

    direction strategy

---
+++ src/main/java/touro/snake/strategy/DirectionStrategy.java
@@ -0,0 +1,36 @@
+package touro.snake.strategy;
+
+import touro.snake.*;
+
+public class DirectionStrategy implements SnakeStrategy {
+
+
+    @Override
+    public void turnSnake(Snake snake, Garden garden) {
+        Direction directions[] = Direction.values();    //list of directions
+
+        Food food = garden.getFood();
+        Square head = snake.getHead();
+
+        //two variables for temp data
+        Direction bestDirection = null;
+        double shortestDistance = Double.MAX_VALUE;
+
+        //loop through directions and get neighbor square, calc distance from neighbor to food, if that distance is better than what we have, save that direction
+        for(Direction d : directions){
+            Square neighbor = head.moveTo(d);
+            double distanceToFood = neighbor.distance(food);
+
+            if(snake.contains(neighbor) || !neighbor.inBounds()){
+                continue;
+            }
+            if(distanceToFood < shortestDistance){
+                bestDirection = d;
+                shortestDistance = distanceToFood;
+            }
+        }
+
+        snake.turnTo(bestDirection);
+
+    }
+}
